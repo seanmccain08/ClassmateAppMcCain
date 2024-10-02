36500374da30b692b7af5d81f3836d85fcc14d89
seanmccain08 <seanmccain327@gmail.com>
2024-10-02 07:26:28 -0500

    Initial Commit

---
+++ ClassmateAppMcCainTests/ClassmateAppMcCainTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClassmateAppMcCainTests.swift
+//  ClassmateAppMcCainTests
+//
+//  Created by SEAN MCCAIN on 10/2/24.
+//
+
+import Testing
+@testable import ClassmateAppMcCain
+
+struct ClassmateAppMcCainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
